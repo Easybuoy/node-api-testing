01e4ffa2e8d21ae0ac005f29ea828e16381ae185
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-08-09 12:44:17 +0100

    setup server

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({status: 'success', message: 'Welcome To Testing API'})
-})
+app.get("/", (req, res) => {
+  res.json({ status: "success", message: "Welcome To Testing API" });
+});
+
+app.post("/add", (req, res) => {
+  const { num1, num2 } = req.body;
+  const add = (num1, num2) => {
+    return num1 + num2;
+  };
+  res.json({
+    status: "success",
+    result: "Welcome To Testing API",
+    result: add(num1, num2)
+  });
+});
 
 const PORT = process.env.PORT || 3000;
 
--- app.test.js
+++ app.test.js
@@ -5,12 +5,19 @@ const chaiHttp = require("chai-http");
 const { expect } = chai;
 chai.use(chaiHttp);
 describe("Server", () => {
-  it("tests home route", async done => {
+  it("welcomes user to the api", async done => {
     let response = await chai.request(app).get("/");
-        expect(response).to.have.status(200);
-        expect(response.body.status).to.equals('success');
-        expect(response.body.message).to.equals('Welcome To Testing API');
+    expect(response).to.have.status(200);
+    expect(response.body.status).to.equals("success");
+    expect(response.body.message).to.equals("Welcome To Testing API");
+    done();
+  });
 
+  it("adds 2 numbers", async done => {
+    let response = await chai.request(app).post("/add").send({num1: 5, num2: 5});
+    expect(response).to.have.status(200);
+    expect(response.body.status).to.equals("success");
+    expect(response.body.result).to.equals(10);
     done();
   });
 });
